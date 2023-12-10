d00fb4e0d38dbea8f83aed21e060a88604ccb4df
3La221 <abacha3la2@gmail.com>
2023-12-10 22:50:44 +0100

    First Commit

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react'
+import  { Row , Col } from 'react-bootstrap'
+import products from '../products'
+import Product from '../components/Product'
+import Loader from '../components/Loader'
+import Messsage from '../components/Message'
+import { useDispatch, useSelector } from 'react-redux'
+import {listProducts} from '../actions/productActions'
+
+
+
+const HomeScreen = () => {
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {error , loading , products} = productList
+
+    
+    useEffect(()=>{
+
+        dispatch(listProducts())
+
+        
+        
+    },[dispatch])
+
+    console.log('loading:',loading)
+  return (
+    <div>
+        <h1>Latest Products</h1>
+        {loading ? <Loader/>
+        : error ? <Messsage variant='danger'>{error}</Messsage>:
+        <Row>
+            
+        {products.map(product => (
+            <Col key={product.id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product}/>
+            </Col>
+        ))}
+    </Row>
+        
+        }
+        
+    </div>
+  )
+}
+
+export default HomeScreen
