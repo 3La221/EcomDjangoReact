bb8e86024e11010a387ebc61e8b850373ae711fa
3La221 <abacha3la2@gmail.com>
2023-12-17 00:01:45 +0100

    '17th'

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,6 +1,43 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product
+
+
+class UserSerializer(serializers.ModelSerializer):
+    
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    is_Admin = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = User 
+        fields = ['_id','id','username','email','name','is_Admin']
+    
+    def get_is_Admin(self,obj):
+        return obj.is_staff
+    
+       
+    def get__id(self,obj):
+        return obj.id
+        
+    def get_name(self,obj):
+        name = obj.first_name
+        if name == '':
+            name = obj.email
+        return name
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['_id','id','username','email','name','is_Admin','token']
+    
+    def get_token(self,obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+    
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-     path('',views.getRoutes,name="routes"),
+     path('users/login/', views.MyTkonObtainPariView.as_view(), name='token_obtain_pair'),
+     path('users/register/', views.registerUser, name='user-register'),
+     path('users/', views.getUsers, name='users'),
+     path('users/profile', views.getUserProfile, name='user-profile'),
      path('products/',views.getProducts,name="products"),
      path('products/<str:pk>',views.getProduct,name="product")
 ]
